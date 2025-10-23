7001f7991115e877d35dca178a4761979dbf7ffb
Carlos Chavez <chavezgomezcarlos@gmail.com>
2025-10-22 21:13:07 -0500

    update

--- product-service/src/main/java/com/tecsup/app/micro/product/client/UserClient.java
+++ product-service/src/main/java/com/tecsup/app/micro/product/client/UserClient.java
@@ -1,6 +1,7 @@
 package com.tecsup.app.micro.product.client;
 
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,19 +21,24 @@ public class UserClient {
     @Value("${user.service.url}")
     private String userServiceUrl;
 
+    @CircuitBreaker(name = "userService", fallbackMethod = "getUserByIdFallback")
     public User getUserById(Long createdBy) {
 
         //String url = "http://localhost:8080/api/users/" + createdBy;
-        String url = userServiceUrl + "/api/users/" + createdBy;
-
-        try{
+            String url = userServiceUrl + "/api/users/" + createdBy;
             User usr = restTemplate.getForObject(url, User.class);
             log.info("User retrievied successfully from userdb: {}", usr);
             return usr;
-        } catch(Exception e){
-            log.error("Error calling user service: {}", e.getMessage());
-            throw new RuntimeException("Error calling User Service: " + e.getMessage());
-        }
+    }
 
+    private User getUserByIdFallback(Long createdBy, Throwable throwable) {
+        log.warn("Fallback method invoked for getUserById due to: {}", throwable.getMessage());
+    return  User.builder()
+            .id(createdBy)
+            .name("Unknow User")
+            .email("Unknow Email")
+            .phone("Unknow Phone")
+            .address("Unknow Address")
+            .build();
     }
 }
