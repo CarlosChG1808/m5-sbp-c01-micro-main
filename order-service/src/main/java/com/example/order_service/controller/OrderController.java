3540f54ffd59ec4adfe39e15330b4bb0441079f1
Carlos Chavez <chavezgomezcarlos@gmail.com>
2025-11-01 23:32:29 -0500

    order complete

--- order-service/src/main/java/com/example/order_service/controller/OrderController.java
+++ order-service/src/main/java/com/example/order_service/controller/OrderController.java
@@ -1,14 +1,13 @@
 package com.example.order_service.controller;
 
+import com.example.order_service.dto.CreateOrderRequest;
 import com.example.order_service.dto.Order;
 import com.example.order_service.service.OrderService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
 @RestController
@@ -23,5 +22,11 @@ public class OrderController {
     public ResponseEntity<Order> getOrderById(@PathVariable Long id) {
         log.info("REST request to get product by id: {}", id);
         return ResponseEntity.ok(orderService.getOrderById(id));
+    }
+    @PostMapping
+    public ResponseEntity<Order> createOrder(@RequestBody CreateOrderRequest request) {
+        log.info("REST request to create order for user: {}", request.getUserId());
+        Order order = orderService.createOrder(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(order);
     }
 }
--- order-service/src/main/java/com/example/order_service/mapper/OrderMapper.java
+++ order-service/src/main/java/com/example/order_service/mapper/OrderMapper.java
@@ -3,9 +3,12 @@ package com.example.order_service.mapper;
 
 import com.example.order_service.client.User;
 import com.example.order_service.dto.Order;
+import com.example.order_service.dto.OrderItem;
 import com.example.order_service.entity.OrderEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface OrderMapper {
@@ -19,6 +22,13 @@ public interface OrderMapper {
     default Order toDomainWithUser(OrderEntity orderEntity, User user) {
         Order order = toDomain(orderEntity);
         order.setCreatedbyUser(user);
+        return order;
+    }
+
+    default Order toDomainWithUserAndItems(OrderEntity orderEntity, List<OrderItem> items, User user) {
+        Order order = toDomain(orderEntity);
+        order.setCreatedbyUser(user);
+        order.setItems(items);
         return order;
     }
 }
